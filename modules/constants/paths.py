d62e0ddac2a24b53597ae4ad903f1b178ea861e9
igorworkman <workout255005@gmail.com>
2025-08-30 15:11:40 +0700

    Update paths.py

--- modules/constants/paths.py
+++ modules/constants/paths.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[2]
+BASE_DIR = Path(__file__).resolve().parents[1]  # корень проекта
 DATA_DIR = BASE_DIR / "data"
 
-# Путь к стартовому фото
 START_PHOTO = DATA_DIR / "start.jpg"
